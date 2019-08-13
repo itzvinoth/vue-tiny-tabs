2a6a48cb577cbb6fa1e10f04fc71a233861ad933
vinothkumarrenganathan <vinoth.vinothrenga@gmail.com>
2019-08-13 20:14:45 +0530

    very simple new build

---
+++ src/plugin.js
@@ -0,0 +1,8 @@
+import VueTinyTabs from './VueTinyTabs.vue';
+
+// module.exports = {
+//   install: function (Vue, options) {
+//     Vue.component('vue-tiny-tabs', VueTinyTabs);
+//   }
+// };
+export default VueTinyTabs;
